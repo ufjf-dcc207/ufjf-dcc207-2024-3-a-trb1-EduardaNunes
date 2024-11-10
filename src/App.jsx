07e69185b09ead3e69c302a0b2e94ed25b83e29a
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-11-10 20:12:39 -0300

    gh-pages added + base tic tac toe struct created

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,68 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Header></Header>
+        <GameCanvas></GameCanvas>
     </>
   )
 }
 
-export default App
+function Header(){
+  return(
+    <h1>Super Tic Tac Toe</h1>
+  )
+}
+
+let gameMatriz = [
+  [null,null,null],
+  [null,null,null],
+  [null,null,null]
+]
+
+let player = 1;
+
+function GameCanvas(){
+  return(
+    <div className='GameCanvas'>
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+      <GameButton></GameButton>
+    </div>
+  )
+}
+
+function GameButton(){
+
+  const [playStatus, SetStatus] = useState(" ");
+
+  const Play = () => {
+    switch(player){
+      case 1:
+        SetStatus("X")
+        player = 2
+        break
+      case 2:
+        SetStatus("O")
+        player = 1
+        break
+      default:
+        console.log("Erro, player não existe")
+        break
+    }
+  }
+
+  return(
+    <button onClick={Play}>{playStatus}</button>
+  )
+}
