a1385dd1016046e64eef31746f3fd8162479e9a7
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-12-14 23:23:24 -0300

    Changed base to igor remote name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-Super-Tic-Tac-Toe",
+  base: "/ufjf-dcc207-2024-3-a-trb1-EduardaNunes",
   plugins: [react()],
 })
