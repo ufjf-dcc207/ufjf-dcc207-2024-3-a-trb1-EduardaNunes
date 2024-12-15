f4c5a0cf120e6eb7eda30557eda7866f93f46dce
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-12-14 22:53:38 -0300

    Updates

--- src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import "./Header.css"
-
-export default function Header(){
-    return(
-      <h1>Tic Tac Toe</h1>
-    )
-  }
--- src/components/RestartButton/RestartButton.jsx
+++
@@ -1,8 +0,0 @@
-import "./RestartButton.css"
-
-export default function RestartButton({onRestartClick, isGameFinished}){
-
-    return(
-        <button className="RestartButton" data-isgamefinished={isGameFinished} onClick={onRestartClick}>Restart</button>
-    )
-}
